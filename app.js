5ccdad498d3ac88d07a634ed0306a7335027971c
n001ce <a1ccormier@gmail.com>
2021-06-27 22:59:13 -0400

    Moved getGuess to play() function

--- app.js
+++ app.js
@@ -1,48 +1,55 @@
 const game = {
-  title: 'Guess the Number!',
-  biggestNum: 100,
-  smallestNum: 1,
-  secretNum: null,
-  prevGuesses: [],
-  play: function() {
-    this.secretNum = Math.floor(Math.random() * (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
-  },
-  render: function(){
-      if(this.getGuess > this.secretNum){
-          alert(`That number is too high enter another. Guessed numbers: ${this.prevGuesses.join(',')}`)
+    title: 'Guess the Number!',
+    biggestNum: 100,
+    smallestNum: 1,
+    secretNum: null,
+    prevGuesses: [],
+    play: function() {
+      this.secretNum = Math.floor(Math.random() * (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
+      this.getGuess = prompt(`Enter a number between ${this.smallestNum} and ${this.biggestNum}`)
+
+      while(true){
+          if(isNaN(this.getGuess)){
+              alert(`Incorrect input type`)
+              this.getGuess = prompt(`Enter a number between ${this.smallestNum} and ${this.biggestNum}`)
+          }else{
+              if(this.getGuess < this.smallestNum || this.getGuess > this.biggestNum){
+                alert(`This number is out of your range. Enter a number between ${this.smallestNum} and ${this.biggestNum}`)
+                }
+              else{
+                  this.prevGuesses.push(this.getGuess)
+                }
+              if(this.getGuess > this.secretNum){
+                  if(this.getGuess < this.biggestNum)
+                      this.biggestNum = this.getGuess
+                      this.render()
+                  this.getGuess = prompt(`Enter a number between ${this.smallestNum} and ${this.biggestNum}`)
+                  }
+              else if(this.getGuess < this.secretNum){
+                  if(this.getGuess > this.smallestNum)
+                      this.smallestNum = this.getGuess
+                      this.render() 
+                  this.getGuess = prompt(`Enter a number between ${this.smallestNum} and ${this.biggestNum}`)
+                  }
+              else{
+                  this.render()
+                  break
+                  }
+          }
       }
-      else{
-          alert(`That number is too low enter another. Guessed numbers: ${this.prevGuesses.join(',')}`)
-      }
-  }
+    },
+    render: function(){
+        if(this.getGuess > this.secretNum){
+            alert(`That number is too high enter another. Guessed numbers: ${this.prevGuesses.join(',')}`)
+        }
+        else if(this.getGuess < this.secretNum){
+            alert(`That number is too low enter another. Guessed numbers: ${this.prevGuesses.join(',')}`)
+        }
+        else{
+            alert(`Congrats!!! You guessed ${this.getGuess} in ${this.prevGuesses.length} tries.
+                  \nguessed numbers: ${this.prevGuesses.join(', ')}`)
+        }
+    }
 }
 
 game.play()
-  
-game.getGuess = prompt(`Enter a number between ${game.smallestNum} and ${game.biggestNum}`)
-
-  while(true){
-      if(isNaN(game.getGuess) || game.getGuess > 100 || game.getGuess < 1){
-          alert(`Incorrect input type`)
-          game.getGuess = prompt(`Enter a number between ${game.smallestNum} and ${game.biggestNum}`)
-      }else{
-          game.prevGuesses.push(game.getGuess)
-          if(game.getGuess > game.secretNum){
-              if(game.getGuess < game.biggestNum)
-                  game.biggestNum = game.getGuess
-                  game.render()
-              game.getGuess = prompt(`Enter a number between ${game.smallestNum} and ${game.biggestNum}`)
-              }
-          else if(game.getGuess < game.secretNum){
-              if(game.getGuess > game.smallestNum)
-                  game.smallestNum = game.getGuess
-                  game.render() 
-              game.getGuess = prompt(`Enter a number between ${game.smallestNum} and ${game.biggestNum}`)
-              }
-          else{
-              alert(`Congrats!!! You guessed ${game.getGuess} in ${game.prevGuesses.length} tries.
-              \nguessed numbers: ${game.prevGuesses.join(', ')}`)
-              break
-              }
-      }
-  }
