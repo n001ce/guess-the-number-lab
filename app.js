288e8e55738c297561e84a07b30d8fa5efd54158
n001ce <a1ccormier@gmail.com>
2021-06-27 23:07:15 -0400

    modified play()

--- app.js
+++ app.js
@@ -9,35 +9,32 @@ const game = {
       this.getGuess = prompt(`Enter a number between ${this.smallestNum} and ${this.biggestNum}`)
 
       while(true){
-          if(isNaN(this.getGuess)){
-              alert(`Incorrect input type`)
-              this.getGuess = prompt(`Enter a number between ${this.smallestNum} and ${this.biggestNum}`)
-          }else{
-              if(this.getGuess < this.smallestNum || this.getGuess > this.biggestNum){
-                alert(`This number is out of your range. Enter a number between ${this.smallestNum} and ${this.biggestNum}`)
-                }
-              else{
-                  this.prevGuesses.push(this.getGuess)
-                }
-              if(this.getGuess > this.secretNum){
-                  if(this.getGuess < this.biggestNum)
-                      this.biggestNum = this.getGuess
-                      this.render()
-                  this.getGuess = prompt(`Enter a number between ${this.smallestNum} and ${this.biggestNum}`)
-                  }
-              else if(this.getGuess < this.secretNum){
-                  if(this.getGuess > this.smallestNum)
-                      this.smallestNum = this.getGuess
-                      this.render() 
-                  this.getGuess = prompt(`Enter a number between ${this.smallestNum} and ${this.biggestNum}`)
-                  }
-              else{
-                  this.render()
-                  break
-                  }
-          }
-      }
-    },
+        if(isNaN(this.getGuess) || this.getGuess < this.smallestNum || this.getGuess > this.biggestNum){
+            alert(`Incorrect input type`)
+            this.getGuess = prompt(`Enter a NUMBER BETWEEN ${this.smallestNum} and ${this.biggestNum}`)
+            this.prevGuesses.push(this.getGuess)
+        }
+        else{
+            this.prevGuesses.push(this.getGuess)
+        }
+        if(this.getGuess > this.secretNum){
+            if(this.getGuess < this.biggestNum)
+                this.biggestNum = this.getGuess
+                this.render()
+            this.getGuess = prompt(`Enter a number between ${this.smallestNum} and ${this.biggestNum}`)
+        }
+        else if(this.getGuess < this.secretNum){
+            if(this.getGuess > this.smallestNum)
+                this.smallestNum = this.getGuess
+                this.render() 
+            this.getGuess = prompt(`Enter a number between ${this.smallestNum} and ${this.biggestNum}`)
+        }
+        else{
+            this.render()
+            break
+        }
+    }
+},
     render: function(){
         if(this.getGuess > this.secretNum){
             alert(`That number is too high enter another. Guessed numbers: ${this.prevGuesses.join(',')}`)
